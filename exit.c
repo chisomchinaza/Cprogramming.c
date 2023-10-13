8aa5abe7e830d217445f702942a34e83fadc84f8
* <chisomchinaza>
2023-10-14 00:43:43 +0100

    exit status check

---
+++ exit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main: this program is to know the exit status of my current process
+ * to be checked with the echo $? command
+ *  Return: 0
+ */
+int 
+main(int ac, char *av[])
+{
+	return (0);
+}
+
